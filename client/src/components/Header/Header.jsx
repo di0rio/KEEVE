2321633aebf395cff6f202bc65fcf4bcf0e02a2c
di0riocaua <caua.diorio29@gmail.com>
2024-10-24 17:05:55 -0300

    realizando header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,29 +3,88 @@ import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
+import logo from "../../assets/logo.png";
+
+// {
+/* {navigation.map((nav) => (
+  <Link
+    className="nav-link nav-item my-3 my-md-none mx-md-3"
+    key={nav.name}
+    to={nav.componente}
+  >
+    {nav.name}
+  </Link>
+))} */
+// }
+
 const Header = () => {
   const navigation = [{ componente: "/users", name: "Usuários" }];
   return (
-    <div className={styles.Main}>
-      <div className={styles.Nav}>
-        {navigation.map((nav) => (
-          <Link
-            className="nav-link nav-item my-3 my-md-none mx-md-3"
-            key={nav.name}
-            to={nav.componente}
-          >
-            {nav.name}
-          </Link>
-        ))}
-      </div>
-      <div>
-        <Link to="/login">
-          <button className={styles.BtnLogin}>
-            <ion-icon name="person-outline"></ion-icon>
-            Entrar
-          </button>
-        </Link>
-      </div>
+    <div className={styles.container}>
+      <nav
+        className="navbar navbar-expand-md top-0 container-fluid px-5"
+        style={{ background: "#160B28" }}
+      >
+        {/* 
+            data-bs-toggle="collapse": ativa a função de Colapso do Bootstrap, que mostra ou esconde um elemento (data-bs-target)
+            data-bs-target="#navbarNav": referencia o elemento que vai ser mostrado ou colapsado
+          */}
+
+        {/* Logo do AquaGuardians */}
+        <a className={styles.keeve} href="/">
+          <img src={logo} alt="" />
+        </a>
+
+        {/* Ícone NavBar no Mobile */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar"
+        >
+          <i className="bi bi-justify"></i>
+        </button>
+
+        {/* Restante da NavBar*/}
+        <div className="offcanvas offcanvas-start" id="offcanvasNavbar">
+          {/* Header no Mobile */}
+          <div className="offcanvas-header px-0">
+            <h5 className="offcanvas-title me-5">Nome do Administrador</h5>
+            <button
+              type="button"
+              className="btn-close ms-5 p-0"
+              data-bs-dismiss="offcanvas"
+            >
+              <i className="bi bi-x-lg" />
+            </button>
+          </div>
+
+          <hr
+            className="my-0 mx-4 d-md-none"
+            style={{ backgroundColor: "#fff" }}
+          />
+
+          {/* Body no Mobile e Links do Nav */}
+          <div className="offcanvas-body nav-underline mx-3 d-md-inline-flex justify-content-center">
+            {navigation.map((nav) => (
+              <Link
+                className="nav-link nav-item my-3 my-md-none mx-md-3"
+                key={nav.name}
+                to={nav.componente}
+              >
+                {nav.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+
+        {/* 
+            Dropdown na direita em desktop
+          */}
+        <div className="d-none d-md-inline-flex">
+          <h5 className="my-auto ms-5 me-3">Nome do Administrador</h5>
+        </div>
+      </nav>
     </div>
   );
 };
