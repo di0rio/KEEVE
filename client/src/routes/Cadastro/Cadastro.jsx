258edb6d1112f56ec365a94e0364db18c062854e
gustavo-lara <gustavolara63719@gmail.com>
2024-10-24 14:08:49 -0300

    Pequenas alt em cadastro e index.html

--- client/src/routes/Cadastro/Cadastro.jsx
+++ client/src/routes/Cadastro/Cadastro.jsx
@@ -100,7 +100,7 @@ const Cadastro = () => {
               type="submit"
               className="w-full py-3 px-4 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
             >
-              ENTRAR
+              CADASTRE-SE
             </button>
           </form>
         </div>
