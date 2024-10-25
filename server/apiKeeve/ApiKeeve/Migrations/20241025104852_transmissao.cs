ab40ea52852e4ae62254216b1396f553c6280cee
gustavo-lara <gustavolara63719@gmail.com>
2024-10-25 07:52:23 -0300

    trasmissao model e controller

---
+++ server/apiKeeve/ApiKeeve/Migrations/20241025104852_transmissao.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiKeeve.Migrations
+{
+    /// <inheritdoc />
+    public partial class transmissao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Trasmissão",
+                table: "Veiculo",
+                newName: "Trasmissao");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Trasmissao",
+                table: "Veiculo",
+                newName: "Trasmissão");
+        }
+    }
+}
